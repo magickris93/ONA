1a717bd522e20d5879ba0ff48adec7c906f2d323
magickris93 <kn334812@students.mimuw.edu.pl>
2016-04-27 17:34:24 +0200

    Code clean-up.

--- Projekt1/ds.py
+++ Projekt1/ds.py
@@ -1,12 +1,13 @@
 import numpy as np
 import random as rand
+import time
 
 
 class Landscape:
     def __init__(self, matrix=None, n=10, sigma=0.1, colorfile=None,
                  hmfile=None, plotfile=None):
         self.rank = n
-        self.size = 2 ** n + 1
+        self.size = (2 ** n) + 1
         self.sigma = sigma
         self.heatmap_color = colorfile
         self.heatmap_file = hmfile
@@ -42,70 +43,12 @@ class Landscape:
                 (point[0], (point[1] - distance) % (self.size - 1))]
 
     def elevate(self):
-        # 1. initiate corner values
-        nw = (0, 0)
-        sw = (self.size - 1, 0)
-        ne = (0, self.size - 1)
-        se = (self.size - 1, self.size - 1)
-
-        val = self.rank * rand.uniform(0.9, 1.1)
-
-        self.set_value(nw, val)
-        self.set_value(ne, val)
-        self.set_value(sw, val)
-        self.set_value(se, val)
-
-        sqrs = [(nw, ne, se, sw)]
-
-        for k in range(self.rank, 0, -1):
-            new = []
-            for sqr in sqrs:
-                c = ((sqr[0][0] + sqr[2][0]) / 2, (sqr[0][1] + sqr[2][1]) / 2)
-                val = np.mean([self.get_value(point) for point in sqr]) + \
-                      2 ** k * self.sigma * np.random.normal()
-
-                self.set_value(c, val)
-
-                rt = (c[0], sqr[2][1])
-                up = (sqr[0][0], c[1])
-                lt = (c[0], sqr[0][1])
-                dn = (sqr[2][0], c[1])
-
-                diff = up[1] - nw[1]
-
-
-'''
-                self.set_value(up, np.mean(
-                    list(map(self.get_value, self.neighbours(up, diff)))) +
-                               2**(k-1)*self.sigma*np.random.normal())
-
-                self.set_value(dn, np.mean(
-                    list(map(self.get_value, self.neighbours(dn, diff)))) +
-                               2**(k-1)*self.sigma*np.random.normal())
-
-                self.set_value(rt, np.mean(
-                    list(map(self.get_value, self.neighbours(rt, diff)))) +
-                               2**(k-1)*self.sigma*np.random.normal())
-
-                self.set_value(lt, np.mean(
-                    list(map(self.get_value, self.neighbours(lt, diff)))) +
-                               2**(k-1)*self.sigma*np.random.normal())
-
-                new.append((nw, up, c, lt))
-                new.append((up, ne, rt, c))
-                new.append((c, rt, se, dn))
-                new.append((lt, c, dn, sw))
-'''
-            sqrs = new
+        pass
 
     def print_map(self):
         print(self.map)
 
 
-# 2. perform diamond step
-# 3. perform square step
-# 4. go deeper
-
-land = Landscape(n=6)
-land.elevate()
-land.print_map()
+t0 = time.time()
+land = Landscape(n=5)
+print('Execution time:', time.time()-t0)
