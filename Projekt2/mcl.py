Fixed column normalization.

Projekt2/mcl.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 import numpy as np
-import scipy as sp
+import scipy.io as sio
 import argparse as ap
 import matplotlib.pyplot as plt
 
 
-class Clusterer:
+class MarkovClusterer(object):
     def __init__(self, format, input_file, epsilon, exponent, depth):
         self.format = format
         self.input_file = input_file
@@ -14,14 +14,11 @@ class Clusterer:
         self.exponent = exponent
         self.depth = depth
         if self.format == 'txt':
-            # text
             self.matrix = np.loadtxt(input_file)
         elif self.format == 'bin':
-            # binary
             self.matrix = np.load(input_file)
         elif self.format == 'mat':
-            # sparse
-            self.matrix = sp.io.loadmat(input_file)
+            self.matrix = sio.loadmat(input_file)
         else:
             raise ValueError('Invalid matrix file format')
 
@@ -29,8 +26,14 @@ class Clusterer:
         return matrix.dot(self.matrix)
 
     def inflate_matrix(self, matrix):
-        new = matrix.power(self.exponent)
-        return new / new.max(axis=0)
+        new = np.power(matrix, self.exponent)
+        return MarkovClusterer.normalize_matrix_columns(new)
+
+    @staticmethod
+    def normalize_matrix_columns(matrix):
+        for i in range(matrix.shape[0]):
+            matrix.T[i] /= np.linalg.norm(matrix.T[i])
+        return matrix
 
     @staticmethod
     def parse_command_line_args():
@@ -53,29 +56,25 @@ class Clusterer:
 
     @staticmethod
     def get_clusterer_from_args():
-        args = Clusterer.parse_command_line_args()
-        return Clusterer(args.format, args.input, args.depth, args.epsilon,
-                         args.exponent)
+        args = MarkovClusterer.parse_command_line_args()
+        return MarkovClusterer(args.format, args.input, args.epsilon, args.exponent,
+                               args.depth)
 
     def mcl(self):
         i = 0
+        norms = []
         current = self.matrix
-        succ = self.square_matrix(current)
-        next_succ = self.inflate_matrix(succ)
-        norms = [np.linalg.norm(next_succ - current)]
-        while i < self.depth and norms[-1] > self.eps:
+        while i < self.depth and (i == 0 or (norms[-1] > self.eps)):
+            tmp = self.square_matrix(current)
+            succ = self.inflate_matrix(tmp)
+            norms.append(np.linalg.norm(succ - current))
             current = succ
-            succ = next_succ
-            next_succ = self.inflate_matrix(succ)
-            norms.append(np.linalg.norm(next_succ - current))
             i += 1
         return current, norms
-# TODO fix loading and saving matrix(sparse), probably csr is the best method
-# TODO to represent them [Efficient powering and multiplication]
+
 
 if __name__ == '__main__':
-    clust = Clusterer.get_clusterer_from_args()
-    print(clust.matrix)
+    clust = MarkovClusterer.get_clusterer_from_args()
     res, norms = clust.mcl()
     ans = input('Show matrix plot? [Y/N]:')
     if ans == 'Y':