cf95b5b84dc6b8348bf2a4fc0743d20c66847773
magickris93 <knagrodkiewicz@gmail.com>
2016-05-29 23:43:21 +0200

    Added support for sparse matrices

--- Projekt2/mcl.py
+++ Projekt2/mcl.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python
 
 import argparse as ap
-
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
+import scipy.sparse as sp
 
 
 class MarkovClusterer(object):
-    def __init__(self, format, input_file, epsilon, exponent, depth):
-        self.format = format
+    def __init__(self, file_type, input_file, epsilon, exponent, depth):
+        self.file_type = file_type
         self.input_file = input_file
         self.eps = epsilon
         self.exponent = exponent
         self.depth = depth
-        if self.format == 'txt':
+        if self.file_type == 'txt':
             self.matrix = np.loadtxt(input_file)
-        elif self.format == 'bin':
+        elif self.file_type == 'bin':
             self.matrix = np.load(input_file)
-        elif self.format == 'mat':
+        elif self.file_type == 'mat':
             self.matrix = sio.loadmat(input_file)
         else:
             raise ValueError('Invalid matrix file format')
+        self.matrix = self.matrix.astype(float)
 
-    def square_matrix(self, matrix):
-        return matrix.dot(self.matrix)
+    @staticmethod
+    def square_matrix(matrix):
+        return matrix.dot(matrix)
 
     def inflate_matrix(self, matrix):
         new = np.power(matrix, self.exponent)
@@ -32,9 +34,16 @@ class MarkovClusterer(object):
 
     @staticmethod
     def normalize_matrix_columns(matrix):
-        for i in range(matrix.shape[0]):
-            matrix.T[i] /= np.linalg.norm(matrix.T[i])
-        return matrix
+        new = matrix.copy()
+        if sp.issparse(new):
+            col_sums = np.array(new.sum(axis=0))[0, :]
+            row_ind, col_ind = new.nonzero()
+            for i in range(len(new.data)):
+                new.data[i] /= col_sums[row_ind[i]]
+        else:
+            col_sums = new.sum(axis=0)
+            new /= col_sums[np.newaxis, :]
+        return new
 
     @staticmethod
     def parse_command_line_args():
@@ -47,7 +56,7 @@ class MarkovClusterer(object):
                             help='input file for matrix')
         parser.add_argument('-f', '--format', required=True, type=str,
                             help='format of matrix input/output')
-        parser.add_argument('-e', '--exponent', required=True, type=int,
+        parser.add_argument('-e', '--exponent', required=True, type=float,
                             help='value of an exponent for matrix inflation')
         parser.add_argument('-d', '--depth', required=True, type=int,
                             help='maximum depth of mcl algorithm')
@@ -61,23 +70,37 @@ class MarkovClusterer(object):
         return MarkovClusterer(args.format, args.input, args.epsilon,
                                args.exponent, args.depth)
 
+    @staticmethod
+    def get_matrix_norm(matrix):
+        norm = 0
+        if sp.issparse(matrix):
+            for element in matrix.data:
+                norm += abs(element)
+        else:
+            for row in matrix:
+                for element in row:
+                    norm += abs(element)
+        return norm
+
     def mcl(self):
         i = 0
         norms = []
         current = MarkovClusterer.normalize_matrix_columns(self.matrix)
         while i < self.depth and (i == 0 or (norms[-1] > self.eps)):
-            tmp = self.square_matrix(current)
+            tmp = MarkovClusterer.square_matrix(current)
             succ = self.inflate_matrix(tmp)
-            norms.append(np.linalg.norm(succ - current))
+            norms.append(MarkovClusterer.get_matrix_norm(succ - current))
             current = succ
-            i += 1
+            i += 2
+            print(norms[-1])
         return current, norms
 
 
 if __name__ == '__main__':
     clust = MarkovClusterer.get_clusterer_from_args()
-    res, norms = clust.mcl()
-    ans = input('Show matrix plot? [Y/N]:')
-    if ans == 'Y':
-        plt.plot(norms)
+    out, norms = clust.mcl()
+    ans = input('Show matrix plot? [y/n]:')
+    if ans == 'y':
+        xs = range(0, 2*len(norms), 2)
+        plt.plot(xs, norms)
         plt.show()
